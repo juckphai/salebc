edf809f36d40876b83f25bca7def46bf9b804bfc
juckphai <llsvuslnw@gmail.com>
2025-11-28 10:56:44 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js - ปรับปรุงเงื่อนไข Fetch สำหรับ CDN
 // --- 1. เปลี่ยนชื่อ Cache เป็นสองเวอร์ชัน ---
-const staticCacheName = 'site-static-v979'; 
-const dynamicCacheName = 'site-dynamic-v979'; 
+const staticCacheName = 'site-static-v949'; 
+const dynamicCacheName = 'site-dynamic-v949'; 
 
 // 2. ไฟล์ที่ต้องการ cache (Assets)
 const assets = [
